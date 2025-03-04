3d2cb4b7279c3f41c72f3d8733e1ea2645b97bd2
alfbrnsy <aldofebriansyah0224@gmail.com>
2025-03-04 14:52:02 +0700

    Last Commit

---
+++ POS/resources/views/products/home_care.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Home Care</title>
+    </head>
+    <body>
+        <h1>Category: Home Care</h1>
+        <p>Daftar produk perawatan rumah tersedia disini.</p>
+    </body>
+</html>
---
+++ POS/resources/views/sales/sales/sales/index.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sales Page</title>
+</head>
+<body>
+    <h1>Sales Transaction</h1>
+    <p>Halaman ini digunakan untuk melakukan transaksi penjualan.</p>
+</body>
+</html>
