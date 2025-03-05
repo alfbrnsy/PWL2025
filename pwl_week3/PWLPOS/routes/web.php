75e002cc2a361a62bbbf2c5617cbbeca45a3b4ee
alfbrnsy <aldofebriansyah0224@gmail.com>
2025-03-05 20:20:48 +0700

    Bagian Query Builder

--- pwl_week3/PWLPOS/routes/web.php
+++ pwl_week3/PWLPOS/routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LevelController;
+use App\Http\Controllers\KategoriController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,3 +20,4 @@ Route::get('/', function () {
 });
 
 Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
