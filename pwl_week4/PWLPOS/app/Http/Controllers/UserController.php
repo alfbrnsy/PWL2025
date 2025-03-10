eded8b74b344f64307c20b8c460c06cacc29430a
alfbrnsy <aldofebriansyah0224@gmail.com>
2025-03-10 22:14:14 +0700

    Bagian B Retrieving Single Models

--- pwl_week4/PWLPOS/app/Http/Controllers/UserController.php
+++ pwl_week4/PWLPOS/app/Http/Controllers/UserController.php
@@ -24,15 +24,29 @@ class UserController extends Controller
 
         //coba akses model UserModel
 
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
+        //$data = [
+       //     'level_id' => 2,
+       //     'username' => 'manager_tiga',
+        //    'nama' => 'Manager 3',
+        //    'password' => Hash::make('12345')
+       // ];
+       // UserModel::create($data);
 
-        $user = UserModel::all(); // ambil semua data
-        return view('user', ['data' => $user]);
+       // $user = UserModel::all(); // ambil semua data
+       // return view('user', ['data' => $user]);
+
+      //$user = UserModel::find(1);
+      //return view ('user', ['data' => $user]);
+
+       //$user = UserModel::firstWhere('level_id', 1);
+      // return view('user', ['data' => $user]);
+
+      //$user = UserModel::findOrFail(1);
+      //return view('user', ['data' => $user]);
+
+      $user = UserModel::where('username', 'manager9')->firsOrFail();
+      return view('user', ['data' => $user]);
+
+
     }
 }
