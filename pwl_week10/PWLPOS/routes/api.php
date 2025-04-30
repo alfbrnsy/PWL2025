46a2bde1fa37bcd97b5ed79084068a5599475bc2
alfbrnsy <aldofebriansyah0224@gmail.com>
2025-04-30 08:31:44 +0700

    Praktikum 3 Membuat Restful API Logout JS10

--- pwl_week10/PWLPOS/routes/api.php
+++ pwl_week10/PWLPOS/routes/api.php
@@ -19,5 +19,5 @@ use App\Http\Controllers\Api\LoginController;
 Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
 Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+    return $request->user();});
+Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
