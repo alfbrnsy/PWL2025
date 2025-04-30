9471fa8554f32b5665cb3b57e6c68c11a5418303
alfbrnsy <aldofebriansyah0224@gmail.com>
2025-04-30 09:06:35 +0700

    Praktikum 4 Implementasi CRUD JS10

--- pwl_week10/PWLPOS/routes/api.php
+++ pwl_week10/PWLPOS/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 
+use App\Http\Controllers\Api\LevelController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\RegisterController;
@@ -21,3 +22,12 @@ Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('lo
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();});
 Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
+
+
+//LevelController
+Route::get('/levels', [LevelController::class,'index']);
+Route::post('/levels', [LevelController::class,'store']);
+Route::get('/levels/{level}', [LevelController::class,'show']);
+Route::put('/levels/{level}', [LevelController::class,'update']);
+Route::delete('/levels/{level}', [LevelController::class,'destroy']);
+
